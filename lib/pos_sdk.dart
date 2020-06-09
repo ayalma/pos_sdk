e61601fbd9b396db4020ba3ceee2efe40a1fd29c
Ali Mohammadi <alimohammadi7117@gmail.com>
2020-06-09 17:03:37 +0430

    اکسپورت موبایل پوز پلاگین

--- lib/pos_sdk.dart
+++ lib/pos_sdk.dart
@@ -1,8 +1,8 @@
 library pos_sdk;
 
 export 'src/widgets/widgets.dart';
+export 'package:mobile_pos_plugin/mobile_pos_plugin.dart';
 export 'package:pos_sdk/src/models/models.dart';
 export 'package:esc_pos_printer/esc_pos_printer.dart' show PosPrintResult;
 export 'package:esc_pos_blue/esc_pos_bluetooth.dart' hide PosPrintResult;
 export 'src/pos_sdk_manager.dart';
-
