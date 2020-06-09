50b39a7553aefc8895ff8d46187694dbc125c379
Ali Mohammadi <alimohammadi7117@gmail.com>
2020-06-09 11:40:47 +0430

    مخفی کردن خروجی کلاس posPrintResult از بلوتوث

--- lib/pos_sdk.dart
+++ lib/pos_sdk.dart
@@ -2,6 +2,6 @@ library pos_sdk;
 
 export 'src/widgets/widgets.dart';
 export 'package:pos_sdk/src/models/models.dart';
-export 'package:esc_pos_blue/esc_pos_bluetooth.dart';
+export 'package:esc_pos_blue/esc_pos_bluetooth.dart' hide PosPrintResult;
 export 'src/pos_sdk_manager.dart';
 
